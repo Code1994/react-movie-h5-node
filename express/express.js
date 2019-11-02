0a867db9f7931e1f164c7de1cc25a1d8905804b5
yangxiaoping <2878847069@qq.com>
2019-11-02 18:50:27 +0800

    feature:配置跨域

--- express/express.js
+++ express/express.js
@@ -2,16 +2,22 @@
 const express = require('express')
 // 引入数据库连接 => 不要在操作数据库时，每次都去连接数据库，否则会造成数据库罢工。应该在初始时连接一次。
 const db = require('../mongoose/mongoose')
+// 引入cors,具体配置方法可见 => https://github.com/expressjs/cors#simple-usage-enable-all-cors-requests
+const cors = require('cors')
 
 // 创建服务器
 const app = express()
-
+app.use(cors())
 // 在数据连接成功后，设置路由
 db.then(res => {
   if (res === 'success') {
-    // 设置路由
+    // 首页
     app.get('/', (req, res) => {
       res.send('index 页面')
+    })
+    // 获取热播电影
+    app.get('/getHitFilms', (req, res) => {
+      res.send('success')
     })
   }
 })
