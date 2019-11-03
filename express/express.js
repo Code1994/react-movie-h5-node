5a1c14f85f8123105dde32a5cfe528aea02d81b0
yangxiaoping <2878847069@qq.com>
2019-11-03 16:23:55 +0800

    feature:添加广告位接口

--- express/express.js
+++ express/express.js
@@ -4,13 +4,17 @@ const express = require('express')
 const db = require('../mongoose/mongoose')
 // 引入cors,具体配置方法可见 => https://github.com/expressjs/cors#simple-usage-enable-all-cors-requests
 const cors = require('cors')
-
+// 引入中间件body-parser, 会将post参数挂载在req.body上
+const parser = require('body-parser')
 // schemas
 const hitFilmModel = require('../mongoose/schema/hitFilms')
+const futureFilmModel = require('../mongoose/schema/futureFilms')
+const adModel = require('../mongoose/schema/ads')
 
 // 创建服务器
 const app = express()
 app.use(cors())
+app.use(parser.json())
 // 在数据连接成功后，设置路由
 db.then(res => {
   if (res === 'success') {
@@ -18,19 +22,55 @@ db.then(res => {
     app.get('/', (req, res) => {
       res.send('index 页面')
     })
-    // 获取热播电影
+    // 1.获取热播电影
     app.get('/getHitFilms', (req, res) => {
       // get方式
       const cinemaCode = req.query.cinemaCode
-      console.log(cinemaCode)
-      hitFilmModel.find({ }, (err, data) => {
-        console.log(data)
+      if (!cinemaCode) {
+        res.send('必传的cinemaCode不存在')
+      }
+      hitFilmModel.find({ cinemaCode }, (err, data) => {
         if (err) {
           throw new Error(err)
         } else {
           const obj = {
             status: 'success',
-            code: '200',
+            code: 200,
+            data
+          }
+          res.send(obj)
+        }
+      })
+    })
+    // 2.获取预售电影
+    app.get('/getFutureFilms', (req, res) => {
+      const cinemaCode = req.query.cinemaCode
+      if (!cinemaCode) {
+        res.send('必传的cinemaCode不存在')
+      }
+      futureFilmModel.find({ cinemaCode }, (err, data) => {
+        if (err) {
+          throw new Error(err)
+        } else {
+          const obj = {
+            status: 'success',
+            code: 200,
+            data
+          }
+          res.send(obj)
+        }
+      })
+    })
+    // 3.获取广告位
+    app.post('/getAds', (req, res) => {
+      // console.log(req.body)
+      adModel.find({}, (err, data) => {
+        if (err) {
+          throw new Error(err)
+        } else {
+          const obj = {
+            status: 'success',
+            code: 200,
             data
           }
           res.send(obj)
---
+++ mongoose/schema/ads.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose')
+
+// Schema属性是一个构造函数
+const Schema = mongoose.Schema
+// 创建一个Schema实例，传入具体的规则参数
+const adSchema = new Schema({
+  cinemaCode: {
+    type: String,
+  },
+  cinemaSystemId: {
+    type: String
+  },
+  id: {
+    type: Number
+  },
+  filmCode: {
+    type: String
+  },
+  filmSystemId: {
+    type: String
+  },
+  name: {
+    type: String
+  },
+  foreignName: {
+    type: String
+  },
+  filmLength: {
+    type: Number
+  },
+  // 影片海报
+  poster: {
+    type: String
+  },
+  wordIntroduction: {
+    type: String
+  },
+  summaryClobId: {
+    type: Number
+  },
+  // 制式
+  dimensionalList: {
+    type: Array
+  },
+  // 热映还是预售 1热映 2预售
+  hitOrPresell: {
+    type: String
+  },
+  // 想看人数
+  wantToSee: {
+    type: String
+  },
+  // 导演
+  director_str: {
+    type: String
+  },
+  // 演员
+  actor_str: {
+    type: String
+  },
+  sort: {
+    type: Number
+  }
+}, {
+  // 注意，这里需要指明集合的名字,下面那里mongoose.model指明的集合名貌似不好用，具体原因待验证。
+  collection: 'ads'
+})
+// 创建模型对象，利用model方法。参数一是数据库名字，参数二是schema
+const adModel = mongoose.model('ads', adSchema,)
+module.exports = adModel
+
